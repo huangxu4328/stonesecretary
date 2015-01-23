ac90914ba99a480d513db02726169b01e92e0d30
huangxu4328 <huangxu4328@gmail.com>
2015-01-23 12:10:15 +0800

    add login.blade.php commit

---
+++ app/views/admin/_layouts/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Stone Secretary_login</title>
+	@include('admin._partials_assets')
+</head>
+<body id="login">
+
+	<div id="login-wrapper" class="png_bg">
+
+		<div id="login-top">
+
+			<h1>Stone Secretary_login</h1>
+
+			<a href="#">
+
+				<img id="logo" src="resources/images/logo.png" alt="Simpla Admin logo" />
+
+			</a> 
+		</div>
+
+		
+	</div>
+</body>
+</html>

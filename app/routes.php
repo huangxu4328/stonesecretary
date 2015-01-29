32f75145e1f4baea5168d39c6f0c7188e42997a5
huangxu4328 <huangxu4328@gmail.com>
2015-01-29 22:05:09 +0800

    20140129

--- app/routes.php
+++ app/routes.php
@@ -11,14 +11,14 @@
 |
 */
 
-//Route::get('/', 'HomeController@showWelcome');
-//Route::get(
-//	'admin/logout',
-//	array(
-//		'as' => 'admin.logout',
-//		'uses' => 'App\Controllers\Admin\AuthController@getLogout'
-//	)
-//);
+Route::get('/', 'HomeController@showWelcome');
+Route::get(
+	'admin/logout',
+	array(
+		'as' => 'admin.logout',
+		'uses' => 'App\Controllers\Admin\AuthController@getLogout'
+	)
+);
 Route::get(
 	'admin/login',
 	array(
@@ -26,22 +26,43 @@ Route::get(
 		'uses' => 'App\Controllers\Admin\AuthController@getLogin'
 	)
 );
-//Route::post(
-//	'admin/login',
-//	array(
-//		'as' => 'admin.login.post',
-//		'uses' => 'App\Controllers\Admin\AuthController@postLogin'
-//	)
-//);
+Route::post(
+	'admin/login',
+	array(
+		'as' => 'admin.login.post',
+		'uses' => 'App\Controllers\Admin\AuthController@postLogin'
+	)
+);
 Route::get(
-	'category',
+	'admin/category',
 	[
 		'as' => 'admin.category',
 		'uses' => 'App\Controllers\Admin\CategoryController@index',
 	]
 );
-//Route::group(array('prefix' => 'admin', 'before' => 'auth.admin'), function()
-//{
-//	Route::any('/', 'App\Controllers\Admin\AuthController@test');
-//	Route::resource('category', 'App\Controllers\Admin\CategoryController');
-//});
+Route::get(
+	'admin/subCategory',
+	[
+		'as' => 'admin.category.subCategory',
+		'uses' => 'App\Controllers\Admin\CategoryController@subCategory',
+	]
+);
+Route::get(
+	'admin/page',
+	[
+		'as' => 'admin.page',
+		'uses' => 'App\Controllers\Admin\PageController@index',
+	]
+);
+Route::group(array('prefix' => 'admin', 'before' => 'auth.admin'), function()
+{
+	Route::any('/', 'App\Controllers\Admin\AuthController@test');
+	Route::resource('category', 'App\Controllers\Admin\CategoryController');
+	Route::resource('page', 'App\Controllers\Admin\PageController');
+});
+
+//前端路由
+Route::get('/{name}', 'HomeController@index');
+Route::get('/{name}/aboutus/{culture}', 'HomeController@aboutus');
+Route::get('/{name}/product/{id}', 'HomeController@productList');
+Route::get('/{name}/product/{id}/{pid}', 'HomeController@productInfo');
